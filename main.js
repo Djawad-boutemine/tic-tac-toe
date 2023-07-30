7409d1dec583762fdcca9ef47a944fa7b3806d38
Djawad-boutemine <138432818+Djawad-boutemine@users.noreply.github.com>
2023-07-30 15:06:39 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,43 +1,59 @@
 
-let gameStates = {
-    first: true,
-    main: {
-        state: false,
-        firstPlayer : "o",
-        one: false,
-        two: false,
-    },
-    final: false,
-};
+let playingModes = {
+    one: true,
+    two: false,
+}
+let starter = "o";
+// pages
 let firstPage = document.querySelector(".first-page");
 let mainPage = document.querySelector(".main-page");
-if (gameStates.first) {
-    let x = document.querySelector(".xo-box button:first-child");
-    console.log(x);
-    let o = document.querySelector(".xo-box button:nth-child(2)");
-    console.log(o);
-    x.onclick = function(){
-        gameStates.main.firstPlayer = "x" ;
-        x.classList.remove("not");
-        o.classList.add("not");
+// function to toggle pages
+let changePage = function (page) {
+    if (page == "first") {
+        firstPage.style.cssText = "display : flex";
+        mainPage.style.cssText = "display : none";
+    } else if (page == "main") {
+        mainPage.style.cssText = "display : grid";
+        firstPage.style.cssText = "display : none";
     }
-    o.onclick = function(){
-        gameStates.main.firstPlayer = "o" ;
-        x.classList.toggle("not");
-        o.classList.toggle("not");
-    }
-    let onePlayer = document.querySelector("#one-player");
-    let twoPlayers = document.querySelector("#two-players");
-    onePlayer.onclick = function () {
-        gameStates.main.one = true;
-        gameStates.main.state = true;
-        firstPage.style.cssText = "display : none ";
-        mainPage.style.cssText = "display : block ";
-    };
-    twoPlayers.onclick = function () {
-        gameStates.main.two = true;
-        gameStates.main.state = true;
-        firstPage.style.cssText = "display : none ";
-        mainPage.style.cssText = "display : block ";
-    };
-} 
+}
+// first page stuff
+let x = document.querySelector(".xo-box button:first-child");
+let o = document.querySelector(".xo-box button:nth-child(2)");
+x.onclick = function () {
+    starter = "x";
+    x.classList.remove("not");
+    o.classList.add("not");
+}
+o.onclick = function () {
+    starter = "o";
+    x.classList.toggle("not");
+    o.classList.toggle("not");
+}
+let onePlayer = document.querySelector("#one-player");
+let twoPlayers = document.querySelector("#two-players");
+
+onePlayer.onclick = function () {
+    changePage("main");
+};
+twoPlayers.onclick = function () {
+    changePage("main");
+    playingModes.one = false;
+    playingModes.two = true;
+};
+// main page stuff
+
+// restart button
+let restart = document.querySelector(".restart");
+restart.addEventListener("click", function () {
+    changePage("first");
+});
+
+
+if (playingModes.one) {
+
+} else if (playingModes.two) {
+
+};
+
+
